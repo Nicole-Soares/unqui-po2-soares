53b0495f3f6c316f2f14658b3ba2701a28b1e138
Nicole-Soares <nicolesoares918@gmail.com>
2025-04-06 13:05:01 -0300

    creacion de la clase rectangulo

---
+++ src/ar/edu/unq/poo2/tp3/Rectangulo.java
@@ -0,0 +1,38 @@
+package ar.edu.unq.poo2.tp3;
+
+public class Rectangulo {
+
+		private int base;
+		private int altura;
+		private Punto puntoOrigen;
+		
+		public Rectangulo(int base, int altura, Punto puntoOrigen) {
+			super();
+			this.base = base;
+			this.altura = altura;
+			this.puntoOrigen = puntoOrigen;
+		}
+		
+		
+		public int getBase() {
+			return base;
+		}
+		public void setBase(int base) {
+			this.base = base;
+		}
+		public int getAltura() {
+			return altura;
+		}
+		public void setAltura(int altura) {
+			this.altura = altura;
+		}
+		public Punto getPuntoOrigen() {
+			return puntoOrigen;
+		}
+		public void setPuntoOrigen(Punto puntoOrigen) {
+			this.puntoOrigen = puntoOrigen;
+		}
+		
+		
+		
+}
